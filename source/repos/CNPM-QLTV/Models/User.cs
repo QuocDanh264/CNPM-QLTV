23e706b67f022a4270b44fff8d85507d0ab3a170
Quoc Danh <danh99171@gmail.com>
2025-04-05 13:30:44 +0700

    Code class User và các thuộc tính

---
+++ source/repos/CNPM-QLTV/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CNPM_QLTV.Models
+{
+    internal class User
+    {
+        public int Id { get; set; }       // Id làm khóa chính
+        public string Username { get; set; }  // Tên đăng nhập
+        public string Password { get; set; }  // Mật khẩu (thường sẽ lưu dưới dạng hash)
+        public string Email { get; set; }     // Địa chỉ email
+    }
+}
